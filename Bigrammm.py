6fd827038d517f4df5b575210ebc7d26e3786ec5
CukoF <36730873+CukoF@users.noreply.github.com>
2023-06-26 20:26:36 +0200

    Update Bigrammm.py
    Adding comment lines

--- Bigrammm.py
+++ Bigrammm.py
@@ -1,17 +1,17 @@
 import nltk
 import string
-
+# Open txt file
 file = open("bigram_list_rel.txt", "w")
-word_data = open('rel_cumlelerr.csv').read()
+# Reaing words from csv file
+word_data = open('words.csv').read()
 
-#for line in word_data:
-    #line = line.strip()
-
+# Create a token from nltk 
 nltk_tokens = nltk.word_tokenize(word_data)
+# Write the created nltk tokens to the file
 file.write(str(list(nltk.bigrams(nltk_tokens))))
 
-#print(list(nltk.bigrams(nltk_tokens)))
+
 
     
-
+# Close the file
 file.close()
